4cd2df479c53a43240c3436480ae075b139c967f
kaloyan-hristov06 <123303570+kaloyan-hristov06@users.noreply.github.com>
2025-02-19 21:38:30 +0200

    add repositories

---
+++ GradesApp/Repositories/StudentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using GradesApp.Data;
+
+namespace GradesApp.Repositories
+{
+    public class StudentsRepository : CrudRepository<Student>, IStudentsRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
