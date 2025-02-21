ca1d5088954e3e14163e69f94603fc45781e899d
nikola-stoyanov06 <nistostoa380@gmail.com>
2025-02-21 17:39:07 +0200

    Add user seeder

---
+++ GradesApp/Data/Seeders/UserSeeder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GradesApp.Data.Seeders
+{
+    public static class UserSeeder
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roleNames = { "Admin", "Teacher", "Student" };
+            foreach (var roleName in roleNames) 
+            { 
+                bool roleExists = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExists) 
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+                var adminuser = new IdentityUser
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+                    EmailConfirmed = true
+                };
+                string password = "Admin#123";
+                var user = await userManager.FindByEmailAsync(adminuser.Email);
+                if (user == null)
+                {
+                    var created = await userManager
+                        .CreateAsync(adminuser, password);
+                    if (created.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(adminuser, "Admin");
+                    }
+                }
+            }
+        }
+    }
+}
--- GradesApp/Program.cs
+++ GradesApp/Program.cs
@@ -1,4 +1,9 @@
 using GradesApp.Data;
+using GradesApp.Data.Seeders;
+using GradesApp.Repositories;
+using GradesApp.Repositories.Abstarctions;
+using GradesApp.Services;
+using GradesApp.Services.Abstractions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,14 +18,31 @@ namespace GradesApp
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            {
+                options.UseSqlServer(connectionString);
+                options.UseLazyLoadingProxies();
+            });
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
-
+            builder.Services.AddScoped<IStudentsRepository, StudentsRepository>();
+            builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
+            builder.Services.AddScoped<ISubjectRepository, SubjectRepository>();
+            builder.Services.AddScoped<IGradeRepository, GradeRepository>();
+            builder.Services.AddScoped<IStudentService, StudentService>();
+            builder.Services.AddScoped<ITeacherService, TeacherService>();
+            builder.Services.AddScoped<ISubjectService, SubjectService>();
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             var app = builder.Build();
+            
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                UserSeeder.Initialize(services).Wait();
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
